a390a7e52f31bc944cce24d1151acedfa738f59e
Yaroslava Myslyva <yaroslava.met@gmail.com>
2022-09-05 17:39:55 +0300

    The up and down buttons work without saving the list

--- app/src/main/java/com/polyglotte/features/allfeatures/FeaturesListAdapter.kt
+++ app/src/main/java/com/polyglotte/features/allfeatures/FeaturesListAdapter.kt
@@ -10,61 +10,81 @@ import androidx.recyclerview.widget.RecyclerView
 import com.polyglotte.R
 import com.polyglotte.app.data.AppFeatureName
 
-class FeaturesListAdapter(private val items: Map<Int, AppFeatureName>) :
-	RecyclerView.Adapter<FeaturesListAdapter.FeatureListItemViewHolder>() {
+class FeaturesListAdapter(private val items: MutableMap<Int, AppFeatureName>) :
+    RecyclerView.Adapter<FeaturesListAdapter.FeatureListItemViewHolder>() {
 
-	inner class FeatureListItemViewHolder(view: View) : RecyclerView.ViewHolder(view), MyClickListener {
-		fun bind(name: String) {
-			itemView.run {
-				findViewById<TextView>(R.id.tv_feature_name).text = name
-				findViewById<Button>(R.id.btn_up).setOnClickListener {
+    inner class FeatureListItemViewHolder(view: View) : RecyclerView.ViewHolder(view),
+        MyClickListener {
+        fun bind(name: String) {
+            itemView.run {
+                findViewById<TextView>(R.id.tv_feature_name).text = name
+                findViewById<Button>(R.id.btn_up).setOnClickListener {
+                    onClickUp(adapterPosition)
+                }
+                findViewById<Button>(R.id.btn_down).setOnClickListener {
+                    onClickDown(adapterPosition)
+                }
+            }
+        }
 
-				}
-				findViewById<Button>(R.id.btn_down).setOnClickListener {
+        override fun onClickUp(p: Int) {
+            if (p != 0){
+                val item = items[p]
+                items[p-1]?.let { items.put(p, it) }
+                if (item != null) {
+                    items[p-1] = item
+                }
+                notifyItemMoved(p, p-1)
+            }
 
-				}
-			}
-		}
+        }
 
-		override fun onClickUp(p: Int) {
-			TODO("Not yet implemented")
-		}
+        override fun onClickDown(p: Int) {
+            if (p != items.size-1){
+                val item = items[p]
+                items[p+1]?.let { items.put(p, it) }
+                if (item != null) {
+                    items[p+1] = item
+                }
+                notifyItemMoved(p, p+1)
+            }
+        }
 
-		override fun onClickDown(p: Int) {
-			TODO("Not yet implemented")
-		}
-	}
+    }
 
-	override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FeatureListItemViewHolder {
-		val view = LayoutInflater.from(parent.context)
-			.inflate(R.layout.lv_item, parent, false)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FeatureListItemViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.lv_item, parent, false)
 
-		return FeatureListItemViewHolder(view)
-	}
+        return FeatureListItemViewHolder(view)
+    }
 
-	override fun onBindViewHolder(holder: FeaturesListAdapter.FeatureListItemViewHolder, position: Int) {
-		val item = items[position]
-		item?.name?.let { holder.bind(it) }
-		holder.itemView.setOnClickListener {
-		//TODO
-		//open Fragment
-		}
-	}
+    override fun onBindViewHolder(
+        holder: FeaturesListAdapter.FeatureListItemViewHolder,
+        position: Int
+    ) {
+        val item = items[position]
+        item?.name?.let { holder.bind(it) }
+        holder.itemView.setOnClickListener {
+            //TODO
+            //open Fragment
+        }
+    }
 
-	override fun getItemCount(): Int = items.size
+    override fun getItemCount(): Int = items.size
 
-	companion object MyDiffUtil : DiffUtil.ItemCallback<AppFeatureName>() {
-		override fun areItemsTheSame(oldItem: AppFeatureName, newItem: AppFeatureName): Boolean {
-			return oldItem == newItem
-		}
+    companion object MyDiffUtil : DiffUtil.ItemCallback<AppFeatureName>() {
+        override fun areItemsTheSame(oldItem: AppFeatureName, newItem: AppFeatureName): Boolean {
+            return oldItem == newItem
+        }
 
-		override fun areContentsTheSame(oldItem: AppFeatureName, newItem: AppFeatureName): Boolean {
-			return oldItem == newItem
-		}
-	}
+        override fun areContentsTheSame(oldItem: AppFeatureName, newItem: AppFeatureName): Boolean {
+            return oldItem == newItem
+        }
+    }
 
-	interface MyClickListener {
-		fun onClickUp(p: Int)
-		fun onClickDown(p: Int)
-	}
+    interface MyClickListener {
+        fun onClickUp(p: Int)
+        fun onClickDown(p: Int)
+    }
 }
--- app/src/main/java/com/polyglotte/features/allfeatures/ui/AllFeaturesFragment.kt
+++ app/src/main/java/com/polyglotte/features/allfeatures/ui/AllFeaturesFragment.kt
@@ -24,87 +24,91 @@ import com.polyglotte.features.allfeatures.FeaturesListAdapter
 
 class AllFeaturesFragment : Fragment() {
 
-	private lateinit var viewModel: AllFeaturesViewModel
+    private lateinit var viewModel: AllFeaturesViewModel
 
-	private var _binding: FragmentAllFeaturesBinding? = null
-	private val binding get() = _binding!!
-	// This property is only valid between onCreateView and onDestroyView.
+    private var _binding: FragmentAllFeaturesBinding? = null
+    private val binding get() = _binding!!
+    // This property is only valid between onCreateView and onDestroyView.
 
-	override fun onCreateView(
-		inflater: LayoutInflater, container: ViewGroup?,
-		savedInstanceState: Bundle?
-	): View {
-		_binding = FragmentAllFeaturesBinding.inflate(inflater, container, false)
-		return binding.root
-	}
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentAllFeaturesBinding.inflate(inflater, container, false)
+        return binding.root
+    }
 
-	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-		super.onViewCreated(view, savedInstanceState)
-		viewModel = ViewModelProvider(this).get(AllFeaturesViewModel::class.java)
-		setupObservers()
-		setupAllFeaturesToolbarMenu()
-		setupRecyclerView()
-		//TODO
-		//or maybe in other commit?
-		//get from prefs
-		//setOnClickListeners (recycler view)
-	}
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel = ViewModelProvider(this).get(AllFeaturesViewModel::class.java)
+        setupObservers()
+        setupAllFeaturesToolbarMenu()
+        setupRecyclerView()
+        //TODO
+        //or maybe in other commit?
+        //get from prefs
+        //setOnClickListeners (recycler view)
+    }
 
-	override fun onDestroyView() {
-		super.onDestroyView()
-		_binding = null
-	}
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 
-	private fun setupObservers() {
-		val centerTextView: TextView = binding.tvCenter
-		viewModel.text.observe(viewLifecycleOwner, {
-			centerTextView.text = it
-		})
+    private fun setupObservers() {
+        val centerTextView: TextView = binding.tvCenter
+        viewModel.text.observe(viewLifecycleOwner, {
+            centerTextView.text = it
+        })
 
-		val networkTextView: TextView = binding.result
-		viewModel.networkResponse.observe(viewLifecycleOwner, {
-			networkTextView.text = it
-		})
-	}
+        val networkTextView: TextView = binding.result
+        viewModel.networkResponse.observe(viewLifecycleOwner, {
+            networkTextView.text = it
+        })
+    }
 
-	private fun setupAllFeaturesToolbarMenu() {
-		val menuHost: MenuHost = requireActivity()
+    private fun setupAllFeaturesToolbarMenu() {
+        val menuHost: MenuHost = requireActivity()
 
-		menuHost.addMenuProvider(object : MenuProvider {
-			override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
-				// Add menu items here
-				menu.clear()
-				val menuItem = AppFeaturesRepository.getFeaturesListItemByKey(AppFeatureName.SETTINGS)
-				menuItem?.run {
-					menu.add(Menu.NONE, menuItem.destinationId, index, menuItem.labelId)
-						.setIcon(menuItem.imageId)
-						.setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS)
-				}
-			}
+        menuHost.addMenuProvider(object : MenuProvider {
+            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                // Add menu items here
+                menu.clear()
+                val menuItem =
+                    AppFeaturesRepository.getFeaturesListItemByKey(AppFeatureName.SETTINGS)
+                menuItem?.run {
+                    menu.add(Menu.NONE, menuItem.destinationId, index, menuItem.labelId)
+                        .setIcon(menuItem.imageId)
+                        .setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS)
+                }
+            }
 
-			override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
-				// Handle the menu selection
-				if (menuItem.itemId == R.id.navigation_settings)
-					findNavController().navigate(R.id.navigation_settings)
-				return true
-			}
-		}, viewLifecycleOwner, Lifecycle.State.RESUMED)
-	}
+            override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+                // Handle the menu selection
+                if (menuItem.itemId == R.id.navigation_settings)
+                    findNavController().navigate(R.id.navigation_settings)
+                return true
+            }
+        }, viewLifecycleOwner, Lifecycle.State.RESUMED)
+    }
 
-	private fun setupRecyclerView() {
-		val prefs: SharedPreferences? = activity?.getSharedPreferences(NavigationActivity.PREFERENCES_NAVIGATION, Context.MODE_PRIVATE)
-		val dataset = prefs?.run { viewModel.getItemsList(this) }
-		binding.list.run {
-			adapter = dataset?.let { FeaturesListAdapter(it) }
-			layoutManager = LinearLayoutManager(context)
-			addItemDecoration(
-				DividerItemDecoration(context, RecyclerView.VERTICAL)
-			)
+    private fun setupRecyclerView() {
+        val prefs: SharedPreferences? = activity?.getSharedPreferences(
+            NavigationActivity.PREFERENCES_NAVIGATION,
+            Context.MODE_PRIVATE
+        )
+        val dataset = prefs?.run { viewModel.getItemsList(this) }
+        binding.list.run {
+            adapter = dataset?.let { FeaturesListAdapter(it as MutableMap<Int, AppFeatureName>) }
+            layoutManager = LinearLayoutManager(context)
+            addItemDecoration(
+                DividerItemDecoration(context, RecyclerView.VERTICAL)
+            )
+        }
 
-		}
-	}
+    }
 
-	companion object {
-		fun newInstance() = AllFeaturesFragment()
-	}
+    companion object {
+        fun newInstance() = AllFeaturesFragment()
+    }
 }
